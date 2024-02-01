58927d714cf70c8280f0f3670280c94f1a1f3440
vish0201 <rabariv832@gmail.com>
2024-02-01 14:19:08 +0530

    'Auto-commit'

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 import os
-import random
 import time
+import subprocess
+
 n = 100
-while n:
-    n-=1
-    file = open("./data.txt","a+")
-    file.write("hello World")
-    file.close()
-    os.system("./upload-karo")
+
+# Replace the path with the actual path to your Git repository
+git_repo_path = r"C:\Users\dvish\react-js\SetOfProjects\furniture-api"
+
+for _ in range(n):
+    with open("./data.txt", "a+") as file:
+        file.write("hello World\n")
+
+    # Use subprocess.Popen to run the Git command in the terminal
+    git_command = "git add . && git commit -m 'Auto-commit' && git push"
+    subprocess.Popen(git_command, shell=True)
+
     time.sleep(10)
